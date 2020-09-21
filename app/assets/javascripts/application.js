32f78a0da3dba3729e3e0c5483f748c3dd4b19c8
Jessica Seo <jessicaseo83@gmail.com>
2020-09-21 17:07:54 +0000

    Add flash messages display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,11 @@
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  // $('.message .close').on('click', function() {
+  //   $(this).closest('.message').transition('fade')
+  //   ;
+  // })
+  $('.message').delay(3000).slideUp();
+
 })
+
